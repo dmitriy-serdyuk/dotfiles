75eadc9a96b914ab96da8b51c7aff97eb309aa26
Dmitriy Serdyuk <serdyuk.dmitriy@gmail.com>
2017-09-06 12:50:17 -0700

    Add symlink for tmux

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ git clone --recursive https://github.com/dmitriy-serdyuk/dotfiles.git
 
 pushd dotfiles
 
+ln -s ~/.tmux/.tmux.conf tmux.conf
 dotfiles --sync -C .dotfilesrc --force
 vim +InstallPlugin +qall
 
